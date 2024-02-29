cc38facd6ec7a4d32930bb1ff2bab55e77d36c43
Ahmed Shawky <130262201+Ahmed-Shawky-xr@users.noreply.github.com>
2024-02-29 13:58:27 +0200

    Update ECPCQ_Template.cpp

--- ECPCQ_Template.cpp
+++ ECPCQ_Template.cpp
@@ -1,14 +1,18 @@
 #include <bits/stdc++.h> // library include everything  
 
-#define mul_mod(a, b, m) (((a % m) * (b % m)) % m)
-#define add_mod(a, b, m) (((a % m) + (b % m)) % m)
 #define ll long long
+#define ull unsigned long long
 #define sz(x) int(x.size())
+#define add_mod(a, b, m) (((a % m) + (b % m)) % m)
+#define sub_mod(a, b, m) (((a % m) - (b % m) + m) % m)
+#define mul_mod(a, b, m) (((a % m) * (b % m)) % m)
+#define fixed(n) cout << fixed << setprecision(n)
 #define all(vec) vec.begin(), vec.end()
 #define rall(vec) vec.rbegin(), vec.rend()
-#define Pi 3.141592653589793
 #define fi first
 #define se second
+#define mod 10000000007
+#define Pi 3.141592653589793
 
 using namespace std;
 
