515cdab15f19d803a2905435615b0dbd4ccf1f74
your_github_username <ahmedshawkyx8@gmail.com>
2024-09-27 01:42:22 +0300

    Update files

--- Bellman_Ford.cpp
+++ Bellman_Ford.cpp
@@ -1,28 +1,39 @@
 template <typename T=int> struct Bellman{
 
     struct Edge{
+
         int from, to;
         T cost;
+
         Edge(int from, int to, T cost) : from(from), to(to), cost(cost) {}
+
     };
 
-
     int n;
-    bool inv = false;
+    bool inv;
     vector<Edge> edges;
     vector<T> dist;
     vector<int> parent;
 
+
     Bellman(int n,bool inv) : n(n), inv(inv) ,dist(n, oo), parent(n, -1) {}
 
-    void addEdge(int from, int to, T cost, bool inv = false){
-        if (inv) edges.emplace_back(from, to, -1LL*cost);
-        else edges.emplace_back(from, to, cost);
+
+    void addEdge(int from, int to, T cost){
+        
+        if (inv) 
+            edges.emplace_back(from, to, -1LL*cost);
+        else 
+            edges.emplace_back(from, to, cost);
+
     }
 
+
     ll bellmanFord(int src){
+
         dist[src] = 0;
-        for(int i = 0; i < sz(edges) - 1; i++){
+
+        for(int i = 0; i < n-1 ; i++){
             for(auto &e : edges){
                 if(dist[e.from] + e.cost < dist[e.to]){
                     dist[e.to] = dist[e.from] + e.cost;
@@ -30,18 +41,20 @@ template <typename T=int> struct Bellman{
                 }
             }
         }
-        if (inv) return dist[n]*-1LL;
-        return dist[n];
+
+        return (inv? dist[n]*-1LL : dist[n]);
     }
 
+
     bool NegCycle(){
-         for (int i=0 ; i< n-1 ; i++){
+
+         for (int i=0 ; i<= n-1 ; i++){
             for(auto &e : edges){
-                if(dist[e.from] + e.cost < dist[e.to]){
+                if (dist[e.from] + e.cost < dist[e.to])
                     return false; // Negative cycle 
-                }
             }
         }
+
         return true;
     }
 
---
+++ DSU.cpp
@@ -0,0 +1,44 @@
+template <typename T> struct DSU{
+
+    public:
+
+        int n;
+        vector <T> parent;
+        vector <T> size;
+
+
+        DSU(int n) : n(n) , parent(n+2), size(n+2) {
+
+            for (int i=1 ; i<=n ; i++){
+                parent[i] = i;
+                size[i] = 1;
+            }
+
+        }
+        
+
+        T get_parent(T u){
+            return parent[u] = (parent[u] == u ? u : get_parent(parent[u])); // path compression
+        }
+
+
+        bool connected(T u, T v){
+            return get_parent(u) == get_parent(v);
+        }
+
+
+        void unoin(T u ,T v){
+            
+            if (connected(u,v)) return;
+
+            int Pu = get_parent(u), Pv = get_parent(v);
+            
+            if (size[Pu] < size[Pv])
+                swap(Pu,Pv);
+
+            size[Pu]+=size[Pv];
+            parent[Pv] = Pu;
+           
+        }
+
+};
