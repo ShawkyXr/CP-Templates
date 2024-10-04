575897f59e70c5f3785ea590e5c24cae37439407
your_github_username <ahmedshawkyx8@gmail.com>
2024-10-05 02:15:43 +0300

    update template

--- Template.cpp
+++ Template.cpp
@@ -1,8 +1,10 @@
+/* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Shawky_XR <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
+
 #include<bits/stdc++.h>
 #include<stdio.h>
- 
+
 using namespace std;
- 
+
 #define nl "\n"
 #define cin(vec) for(auto& i : vec) cin >> i
 #define cin_2d(vec, n, m) for(int i = 0; i < n; i++) for(int j = 0; j < m && cin >> vec[i][j]; j++);
@@ -19,45 +21,38 @@ using namespace std;
 #define fixed(n) cout << fixed << setprecision(n)
 #define all(vec) vec.begin(), vec.end()
 #define rall(vec) vec.rbegin(), vec.rend()
+#define mod 1000000007
 #define Pi 3.141592653589793
 typedef long long ll;
 typedef unsigned long long ull;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 typedef vector<long long> vll;
-constexpr ll oo = 1LL << 60;
+const ll oo = 1LL << 60;
 constexpr int Mod =  1e9 + 7;
- 
-template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
-    for (auto &x : v) in >> x;
-    return in;
+
+
+void IO(){
+   #ifndef ONLINE_JUDGE
+       freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout); 
+   #endif
+   ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+   cerr << "Time Taken: " << (float)clock() / CLOCKS_PER_SEC << " Secs" << "\n";
 }
- 
-template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) { 
-    for (const T &x : v) out << x << ' '; 
-    return out;
-}
- 
-void Shawky(){
-  ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-  #ifndef ONLINE_JUDGE
-      freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout); 
-  #endif
-  cerr << "Time Taken: " << (float)clock() / CLOCKS_PER_SEC << " Secs" << "\n";
-}
- 
- 
+
+
 void solve(){
- 
+
 }
 
 
-signed main(){
-  Shawky();
-  int testcase=1 ;  
-  // cin >> testcase;
-  while (testcase--) {
-    solve();
-  }
-  return 0;
+int main(){
+    IO();
+    int testcase=1 ;
+    // cin >> testcase;
+    for (int t=1 ; t<=testcase ; t++) {
+        // cout << "Case #" << testcase << ": ";
+        solv();
+    }
+    return 0;
 }
